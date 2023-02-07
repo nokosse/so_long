051e931896cb2f8bccbd8398c66899d8f64e31f2
nokosse <visouthivong.kevin@gmail.com>
2023-02-07 18:52:17 +0100

    changed values

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 12:16:22 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/02/07 18:29:54 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/02/07 18:51:54 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(void)
 {
 	//open a window
 	void *mlx = mlx_init();
-    void *win = mlx_new_window(mlx, 640, 360, "Tutorial Window");
+    void *win = mlx_new_window(mlx, 640, 360, "so_long");
     mlx_loop(mlx);
     (void)win;
 }
