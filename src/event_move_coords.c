7ae944741fb0208153e31cf09f7dd3bb670801b5
root <root@LAPTOP-3PG926D4.localdomain>
2023-02-20 09:55:02 +0000

    added file

---
+++ src/event_move_coords.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   event_move_coords.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 09:51:53 by marvin            #+#    #+#             */
+/*   Updated: 2023/02/20 09:51:53 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/so_long.h"
+
+// This function will get the coordinates of the player on the map.
+// The coordinates of the player are located at game.goblin.x or y.
+// The coordinates of the player is P on maps.
+// The coordinates 0, 0 are map[0][0] on the array.
+// The coordinates at the right of 0, 0 are map[0][1]
+int	get_player_x(t_game game)
+{
+	
+}
+
+int	get_player_y(t_game game)
+{
+
+}
