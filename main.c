491e2f901b87031e845844aedf258d465a590422
nokosse <visouthivong.kevin@gmail.com>
2023-02-28 13:22:56 +0100

    cross to quit working

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 12:16:22 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/02/27 19:29:03 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/02/28 13:22:36 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ int	main(int ac, char **av)
 	game.map_dim = get_dims(get_map_height(av), get_map_width(av) - 1);
 	map_init(&game);
 	mlx_loop_hook(game.mlx, &render_next_frame, &game);
+	mlx_hook(game.win, DestroyNotify, 0, &destroy_window, &game);
 	mlx_hook(game.win, KeyPress, KeyPressMask, &handle_keypress, &game);
 	mlx_loop(game.mlx);
 	return (0);
 }
 
-// TODO : red cross close window
 // TODO : new algorithm to check path (flood fill ?)
 //        check if player can reach E and all C
