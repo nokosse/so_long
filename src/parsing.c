2b5309a1c9373c2354e025d41110a0e8d703f9f6
nokosse <visouthivong.kevin@gmail.com>
2023-03-02 16:35:43 +0100

    fixed leaks

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 12:16:43 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/03/02 16:16:05 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/03/02 16:33:56 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,8 +111,9 @@ int	check_map(int ac, char **av)
 	char	**map;
 	char	**map_copy;
 
-	if (!(map_file_exist(ac, av)) || check_empty(av) || !(check_map_width(av))
-		|| !(check_map_junk(av)))
+	if (map_file_exist(ac, av) == 0 || check_map_junk(av) == 0)
+		return (0);
+	if (!(map_file_exist(ac, av)) || check_empty(av) || !(check_map_width(av)))
 		return (0);
 	height = get_map_height(av);
 	width = get_map_width(av) - 1;
